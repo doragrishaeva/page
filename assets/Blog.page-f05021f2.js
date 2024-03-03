a37038f1decf605498f6a63b87bac4951a4ba737
doragrishaeva <59879835+doragrishaeva@users.noreply.github.com>
2024-03-03 19:57:59 +0100

    Updates

---
+++ assets/Blog.page-f05021f2.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-02372208.js";const t=()=>o.jsx("div",{children:"Blog"});export{t as BlogPage};
---
+++ assets/Settings.page-e6286ac5.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-02372208.js";const r=()=>t.jsx("div",{children:"Settings"});export{r as SettingsPage};
